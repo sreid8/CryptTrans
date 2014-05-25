73f98de536321d7c6613adab587a4dc83868fb71
Sean Reid <sean.reid8@gmail.com>
2014-05-25 16:00:15 -0400

    This is a second commit to make sure that the connection to GitHub is working correctly.

---
+++ CryptTrans/src/com/sreid/CryptTrans/TempMain.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.sreid.CryptTrans;
+
+/**
+ * @author sean
+ * 
+ * This is test class made to make sure that the github connection is setup correctly
+ *
+ */
+public class TempMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!");
+		
+	}
+
+}
