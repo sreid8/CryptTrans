004b9d808b9be8074a13c98dc8be8ba2de7b3ff3
sreid <sean.reid8@gmail.com>
2014-06-13 17:01:46 -0400

    Started implementing TcpSocketFactory

--- CryptTrans/src/com/sreid/CryptTrans/TcpSocketFactory.java
+++ CryptTrans/src/com/sreid/CryptTrans/TcpSocketFactory.java
@@ -8,20 +8,49 @@ import java.net.UnknownHostException;
 import javax.net.SocketFactory;
 
 public class TcpSocketFactory extends SocketFactory {
-
+	/**
+	 *  createSocket preferred class. 
+	 *  @param String ip - the remote IP Address
+	 *  @param int port - the remote Port
+	 *  @returns Socket - the TcpSocket
+	 */
 	@Override
-	public Socket createSocket(String arg0, int arg1) throws IOException,
+	public Socket createSocket(String ip, int port) throws IOException,
 			UnknownHostException {
 		// TODO Auto-generated method stub
-		return null;
+		Socket socket;
+		try {
+			socket = new Socket(ip, port);
+		}
+		catch (IOException e) {
+			throw e;
+		}
+		return socket;
 	}
 
+	/**
+	 *  createSocket preferred class. 
+	 *  @param InetAddress ip - the remote IP Address
+	 *  @param int port - the remote Port
+	 *  @returns Socket - the TcpSocket
+	 */
 	@Override
-	public Socket createSocket(InetAddress arg0, int arg1) throws IOException {
+	public Socket createSocket(InetAddress ip, int port) throws IOException {
 		// TODO Auto-generated method stub
-		return null;
+		Socket socket;
+		try {
+			socket = new Socket(ip, port);
+		}
+		catch (IOException e) {
+			throw e;
+		}
+		return socket;
 	}
 
+	/**
+	 *  @deprecated
+	 *  Do not use, not implemented.. probably should though, that UnknownHostException would be nice
+	 */
 	@Override
 	public Socket createSocket(String arg0, int arg1, InetAddress arg2, int arg3)
 			throws IOException, UnknownHostException {
@@ -29,6 +58,10 @@ public class TcpSocketFactory extends SocketFactory {
 		return null;
 	}
 
+	/**
+	 * @deprecated
+	 * Do not use, not implemented
+	 */
 	@Override
 	public Socket createSocket(InetAddress arg0, int arg1, InetAddress arg2,
 			int arg3) throws IOException {
